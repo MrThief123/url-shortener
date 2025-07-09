9473adfe6dfc60938d3c0072cc214133d2d1c6c6
MrThief123 <90836480+MrThief123@users.noreply.github.com>
2025-07-09 14:46:00 +1000

    fix: auth issue

--- src/db.js
+++ src/db.js
@@ -3,16 +3,16 @@ require('dotenv').config()
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    user: process.env.DB_USER || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'url_shortener',
-    password: process.env.DB_PASSWORD || 'password',
-    port: process.env.DB_PORT || 5432,
-    ssl: false
-    // ssl: {
-    //     rejectUnauthorized: false // Allow self-signed AWS certificates
-    // }
-})
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 
 // Test the database connection
 pool.connect()
